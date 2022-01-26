79f97e227dd4bb18dec29d8fc8cd47e1727d41b9
sanjo lawal <sanjolawal600@gmail.com>
2022-01-25 21:02:27 -0800

    ch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import Header from './Header/Header';
 import Main from './Main/Main';
@@ -9,7 +9,8 @@ import Blog from './Blog/Blog';
 
 function App() {
   return (
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
+  //  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <div>
    <Navbar/>
    <Header/>
    <Main/>
@@ -17,8 +18,8 @@ function App() {
   <Routes>
     <Route path="/Blog" element={<Blog/>}/>
   </Routes>
-
-   </BrowserRouter>
+</div>
+  //  </BrowserRouter>
   );
 }
 
